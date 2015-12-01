0b4ec26ab4f0eeefb2d03d29f872d798dcc5845a
wangyuesong <573978301@qq.com>
2015-11-30 18:10:16 -0800

    Change change

--- VuforiaSamples-5-0-5/app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/app/ImageTargets/Object3D.java
+++ VuforiaSamples-5-0-5/app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/app/ImageTargets/Object3D.java
@@ -1,41 +1,97 @@
 package com.qualcomm.vuforia.samples.VuforiaSamples.app.ImageTargets;
 
 
-import android.opengl.Matrix;
+import android.graphics.CornerPathEffect;
 
 import com.qualcomm.vuforia.samples.SampleApplication.utils.CubeObject;
-import com.qualcomm.vuforia.samples.SampleApplication.utils.MeshObject;
 
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yuesongwang on 11/29/15.
  */
 public class Object3D {
+    public static int SCALE;
+
     public CubeObject cube;
-    public List<float[]>CenterList;
+    public List<int[]> offsetList;
     private double length, width, height;
-    public float Center_X;
-    public float Center_Y;
-    public float Center_Z;
+    public int centerX;
+    public int centerY;
+    public int centerZ;
     public boolean isSticked;
     public boolean lock;
     public float[] selfRotationMatrix;
     public int textureId;
     public int interval;
 
-    public Object3D(float x, float y, float z,int t,List<float[]>c) {
-//        selfRotationMatrix = new float[16];
-//        Matrix.setIdentityM(selfRotationMatrix,0);
-//        interval = 50;
-        this.length = length;
-        this.width = width;
-        this.height = height;
-        this.Center_X = x;
-        this.Center_Y = y;
-        this.Center_Z = z;
-        this.CenterList = c;
+    public Object3D(int x, int y, int z,int t,List<int[]>c) {
+        this.centerX = x;
+        this.centerY = y;
+        this.centerZ = z;
+        this.offsetList = c;
         this.textureId =t;
         cube = new CubeObject();
     }
+
+    private Map<Coordinate2D,Integer> findLowestBottom()
+    {
+        HashMap<Coordinate2D,Integer> coordinateAndZMap = new HashMap<>();
+        for(int[] offsetXYZ: offsetList) {
+            Coordinate2D coord = new Coordinate2D(centerX+ offsetXYZ[0],centerY + offsetXYZ[1]);
+            if(!coordinateAndZMap.containsKey(coord))
+                coordinateAndZMap.put(coord,offsetXYZ[2]);
+            else {
+                if (offsetXYZ[2] < coordinateAndZMap.get(coord))
+                    coordinateAndZMap.put(coord, offsetXYZ[2]);
+            }
+        }
+        return coordinateAndZMap;
+    }
+
+    public static void main(String[] args)
+    {
+        Object3D o = new Object3D(0,0,0,8,new ArrayList<int[]>());
+        ArrayList<int[]> arrayList = new ArrayList<>();
+        arrayList.add(new int[]{1,1,1});
+        arrayList.add(new int[]{1,1,-1});
+        arrayList.add(new int[]{-1,1,1});
+        arrayList.add(new int[]{-1,1,2});
+        arrayList.add(new int[]{0,0,0});
+
+        o.offsetList = arrayList;
+        Map<Coordinate2D,Integer>  a = o.findLowestBottom();
+        for(Coordinate2D key:a.keySet())
+        {
+            System.out.println(a.get(key));
+        }
+    }
+
+    private static HashMap<Integer,boolean[][]> levelOccupation;
+
+    public static class Coordinate2D{
+        @Override
+        public boolean equals(Object o) {
+            Coordinate2D c = (Coordinate2D)o;
+            return (c.X == this.X) && (c.Y == this.Y);
+        }
+
+        @Override
+        public int hashCode() {
+            return X * 100000 + Y;
+        }
+
+        public Coordinate2D(int x, int y) {
+            X = x;
+            Y = y;
+        }
+
+        public int X;
+        public int Y;
+    }
+
 }
